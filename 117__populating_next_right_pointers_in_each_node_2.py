03b01efd06dcaee7766b34ab624d534359d82afa
thephilosoft <thephilosoft@gmail.com>
2023-01-04 22:40:19 -0500

    117: O(1) space solution

--- 117__populating_next_right_pointers_in_each_node_2.py
+++ 117__populating_next_right_pointers_in_each_node_2.py
@@ -5,7 +5,38 @@ from lib.TreeNode import TreeNode
 
 
 class Solution:
-    def connect(self, root: TreeNode) -> Optional[TreeNode]:
+    def connect(selfself, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        left = TreeNode()
+        leftmost = root.left if root.left else root.right
+        if root.left and root.right:
+            root.left.next = root.right
+
+        parent = leftmost
+
+        while parent:
+            if not leftmost:
+                leftmost = parent.left if parent.left else parent.right
+
+            if parent.left:
+                left.next = parent.left
+                left = parent.left
+            if parent.right:
+                left.next = parent.right
+                left = parent.right
+
+            if parent.next:
+                parent = parent.next
+            else:
+                parent = leftmost
+                leftmost = None
+                left = TreeNode()
+
+        return root
+
+    def connectBFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
 
