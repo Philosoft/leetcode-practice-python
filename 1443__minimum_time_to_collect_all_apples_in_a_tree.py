a2b7340d7177187559a3ddfa1febea9a20ab3822
thephilosoft <thephilosoft@gmail.com>
2023-01-11 09:45:42 -0500

    1443: Minimum time to collect all apples in a tree

---
+++ 1443__minimum_time_to_collect_all_apples_in_a_tree.py
@@ -0,0 +1,110 @@
+from collections import defaultdict
+from typing import List, Tuple
+from unittest import TestCase
+
+"""
+1443. Minimum Time to Collect All Apples in a Tree
+
+Given an undirected tree consisting of n vertices numbered from 0 to n-1, which has some apples in their vertices.
+You spend 1 second to walk over one edge of the tree. Return the minimum time in seconds you have to spend to collect
+all apples in the tree, starting at vertex 0 and coming back to this vertex.
+
+The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge
+connecting the vertices ai and bi. Additionally, there is a boolean array hasApple, where hasApple[i] = true means that
+vertex i has an apple; otherwise, it does not have any apple.
+
+
+Example 1:
+
+Input:
+    n = 7
+    edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]]
+    hasApple = [false,false,true,false,true,true,false]
+Output: 8 
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect
+all apples is shown by the green arrows.  
+
+
+Example 2:
+
+Input:
+    n = 7,
+    edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]],
+    hasApple = [false,false,true,false,false,true,false]
+Output: 6
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect
+all apples is shown by the green arrows.  
+
+
+Example 3:
+
+Input:
+    n = 7,
+    edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]],
+    hasApple = [false,false,false,false,false,false,false]
+Output: 0
+ 
+Constraints:
+
+1 <= n <= 105
+edges.length == n - 1
+edges[i].length == 2
+0 <= ai < bi <= n - 1
+from_i < to_i
+hasApple.length == n
+"""
+
+
+class Solution(TestCase):
+    def test_example_1(self):
+        n = 7
+        edges = [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]]
+        hasApple = [False, False, True, False, True, True, False]
+
+        self.assertEqual(8, self.minTime(n, edges, hasApple))
+
+    def test_example_2(self):
+        n = 7
+        edges = [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]]
+        hasApple = [False, False, True, False, False, True, False]
+
+        self.assertEqual(6, self.minTime(n, edges, hasApple))
+
+    def test_example_3(self):
+        n = 7
+        edges = [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]]
+        hasApple = [False, False, False, False, False, False, False]
+
+        self.assertEqual(0, self.minTime(n, edges, hasApple))
+
+    def test_first_failing_leetcode(self):
+        n = 4
+        edges = [[0, 2], [0, 3], [1, 2]]
+        hasApple = [False, True, False, False]
+
+        self.assertEqual(4, self.minTime(n, edges, hasApple))
+
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        adj = defaultdict(set)
+        for a, b in edges:
+            adj[a].add(b)
+            adj[b].add(a)
+
+        visited = set()
+
+        def dfs(start: int) -> Tuple[bool, int]:
+            if start in visited:
+                return False, 0
+
+            visited.add(start)
+            total_time = 0
+            has_apple = hasApple[start]
+            for child in adj[start]:
+                (child_has_apple, time) = dfs(child)
+                if child_has_apple:
+                    total_time += time + 2
+                    has_apple = True
+
+            return has_apple, total_time
+
+        return dfs(0)[1]
