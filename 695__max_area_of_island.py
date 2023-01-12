51894690d66fce62678e4507cea9719c0707b43a
thephilosoft <thephilosoft@gmail.com>
2023-01-12 14:51:27 -0500

    695: add dfs solution

--- 695__max_area_of_island.py
+++ 695__max_area_of_island.py
@@ -10,18 +10,22 @@ class Solution(unittest.TestCase):
                 [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
         self.assertEqual(6, self.maxAreaOfIsland(grid))
+        self.assertEqual(6, self.maxAreaOfIslandDFS(grid))
 
     def test_example_2(self):
         grid = [[0, 0, 0, 0, 0, 0, 0, 0]]
         self.assertEqual(0, self.maxAreaOfIsland(grid))
+        self.assertEqual(0, self.maxAreaOfIslandDFS(grid))
 
     def test_single_island(self):
         grid = [[0, 0, 1, 0, 0]]
         self.assertEqual(1, self.maxAreaOfIsland(grid))
+        self.assertEqual(1, self.maxAreaOfIslandDFS(grid))
 
     def test_square_island(self):
         grid = [[1, 1], [1, 1], ]
         self.assertEqual(4, self.maxAreaOfIsland(grid))
+        self.assertEqual(4, self.maxAreaOfIslandDFS(grid))
 
     def test_max_island(self):
         grid = [[1] * 50] * 50
@@ -62,3 +66,26 @@ class Solution(unittest.TestCase):
                     answer = max(answer, area_of_island(row, column))
 
         return answer
+
+    def maxAreaOfIslandDFS(self, grid: List[List[int]]) -> int:
+        m = 0
+
+        def area_of_island(row: int, column: int) -> int:
+            if row < 0 or column < 0 or row >= len(grid) or column >= len(grid[row]) or grid[row][column] != 1:
+                return 0
+
+            grid[row][column] = 2
+            return 1 + sum([area_of_island(r, c) for r, c in
+                         [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]])
+
+        for row in range(len(grid)):
+            for column in range(len(grid[row])):
+                if grid[row][column] == 1:
+                    m = max(m, area_of_island(row, column))
+
+        for row in range(len(grid)):
+            for column in range(len(grid[row])):
+                if grid[row][column] == 2:
+                    grid[row][column] = 1
+
+        return m
