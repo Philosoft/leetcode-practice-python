b2211f1fdab728b832939668fe91b17d745839f3
thephilosoft <thephilosoft@gmail.com>
2023-01-14 16:04:55 -0500

    1061: add normal disjoint set / union find solutions

--- 1061__lexicographically_smallest_equivalent_string.py
+++ 1061__lexicographically_smallest_equivalent_string.py
@@ -1,17 +1,81 @@
 import collections
 import unittest
-from typing import Set
+from typing import Set, List
 
 
 class Solution(unittest.TestCase):
     def test_example_1(self):
         self.assertEqual("makkek", self.smallestEquivalentStringFirstSolution("parker", "morris", "parser"))
+        self.assertEqual("makkek", self.smallestEquivalentStringDisjointSet("parker", "morris", "parser"))
+        self.assertEqual("makkek", self.smallestEquivalentStringUF("parker", "morris", "parser"))
 
     def test_example_2(self):
         self.assertEqual("hdld", self.smallestEquivalentStringFirstSolution("hello", "world", "hold"))
+        self.assertEqual("hdld", self.smallestEquivalentStringDisjointSet("hello", "world", "hold"))
+        self.assertEqual("hdld", self.smallestEquivalentStringUF("hello", "world", "hold"))
 
     def test_example_3(self):
         self.assertEqual("aauaaaaada", self.smallestEquivalentStringFirstSolution("leetcode", "programs", "sourcecode"))
+        self.assertEqual("aauaaaaada", self.smallestEquivalentStringDisjointSet("leetcode", "programs", "sourcecode"))
+        self.assertEqual("aauaaaaada", self.smallestEquivalentStringUF("leetcode", "programs", "sourcecode"))
+
+    def smallestEquivalentStringUF(self, s1: str, s2: str, baseStr: str) -> str:
+        # Union find version
+        uf = {}
+
+        def find(char: str) -> str:
+            if char not in uf:
+                uf[char] = char
+
+            if uf[char] == char:
+                return char
+
+            return find(uf[char])
+
+        def union(left: str, right: str) -> None:
+            root1, root2 = find(left), find(right)
+
+            if root1 > root2:
+                root1, root2 = root2, root1
+
+            uf[root2] = root1
+
+        for a, b in zip(s1, s2):
+            union(a, b)
+
+        return "".join([find(char) for char in baseStr])
+
+
+    def smallestEquivalentStringDisjointSet(self, s1: str, s2: str, baseString: str) -> str:
+        """
+        Idea. Construct disjoint set. Always lexicographically smaller letter as root.
+        Then go through base string taking roots
+
+        @todo: rewrite with hashset instead of array
+        """
+        dis_set: List[int] = [i for i in range(26)]
+
+        def union(left: str, right: str) -> None:
+            root1, root2 = find(left), find(right)
+            if root1 == root2:
+                return
+
+            if root1 > root2:
+                root1, root2 = root2, root1
+
+            dis_set[ord(root2) - ord("a")] = ord(root1) - ord('a')
+
+        def find(target: str) -> str:
+            t = ord(target) - ord("a")
+            if dis_set[t] == t:
+                return target
+
+            return find(chr(dis_set[t] + ord("a")))
+
+        for a, b in zip(s1, s2):
+            union(a, b)
+
+        return "".join([min(x, find(x)) for x in baseString])
 
     def smallestEquivalentStringFirstSolution(self, s1: str, s2: str, baseStr: str) -> str:
         equiv_map = collections.defaultdict(set)
