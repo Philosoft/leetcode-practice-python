e6b3e97b8830ca9e7d07859ba89bed946904f354
thephilosoft <thephilosoft@gmail.com>
2023-02-28 09:57:54 -0500

    0652: find duplicate subtrees

---
+++ 0652__find_duplicate_subtrees.py
@@ -0,0 +1,27 @@
+"""
+LeetCode: https://leetcode.com/problems/find-duplicate-subtrees/description/
+"""
+from collections import defaultdict
+from typing import Optional, List, Dict
+
+from lib.TreeNode import TreeNode
+
+
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        hash_to_roots: Dict[str, List[TreeNode]] = defaultdict(list)
+
+        def dfs(node: Optional[TreeNode]) -> str:
+            if not node:
+                return 'x'
+
+            tree_hash = '(' + '(' + dfs(node.left) + ')' + f'({node.val})' + '(' + dfs(node.right) + ')' + ')'
+            hash_to_roots[tree_hash].append(node)
+
+            return tree_hash
+
+        dfs(root)
+
+        print(hash_to_roots)
+
+        return list(map(lambda x: x[0], filter(lambda x: len(x) > 1, hash_to_roots.values())))
